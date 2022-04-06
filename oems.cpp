6479e4b20915b9f127483befbd003487ef940514
adosix <andrejjezik@gmail.com>
2022-04-06 18:42:49 +0200

    refactor of merging function

--- oems.cpp
+++ oems.cpp
@@ -1,139 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <mpi.h>
+
+#include "oems.h"
 using namespace std;
 
-void oddEvenMerge(int my_rank, int recievedNumbers[2])
+void oddEvenMerge(int my_rank, int processInput[2])
 {
     int min, max;
-    if (recievedNumbers[0] < recievedNumbers[1])
+    if (processInput[0] < processInput[1])
     {
-        min = recievedNumbers[0];
-        max = recievedNumbers[1];
+        min = processInput[0];
+        max = processInput[1];
     }
     else
     {
-        min = recievedNumbers[1];
-        max = recievedNumbers[0];
+        min = processInput[1];
+        max = processInput[0];
     }
-
-    switch (my_rank)
-    {
-    case 0:
-    {
-        MPI_Send(&min, 1, MPI_INT, 4, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 5, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 1:
-    {
-        MPI_Send(&min, 1, MPI_INT, 4, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 5, 1, MPI_COMM_WORLD);
-        break;
-    }
-    case 2:
-    {
-        MPI_Send(&min, 1, MPI_INT, 6, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 7, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 3:
-    {
-        MPI_Send(&min, 1, MPI_INT, 6, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 7, 1, MPI_COMM_WORLD);
-        break;
-    }
-    case 4:
-    {
-        MPI_Send(&min, 1, MPI_INT, 10, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 8, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 5:
-    {
-        MPI_Send(&min, 1, MPI_INT, 8, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 13, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 6:
-    {
-        MPI_Send(&min, 1, MPI_INT, 10, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 9, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 7:
-    {
-        MPI_Send(&min, 1, MPI_INT, 9, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 13, 1, MPI_COMM_WORLD);
-        break;
-    }
-    case 8:
-    {
-        MPI_Send(&min, 1, MPI_INT, 12, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 11, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 9:
-    {
-        MPI_Send(&min, 1, MPI_INT, 12, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 11, 1, MPI_COMM_WORLD);
-        break;
-    }
-    case 10:
-    {
-        MPI_Send(&min, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 14, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 11:
-    {
-        MPI_Send(&min, 1, MPI_INT, 14, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 18, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 12:
-    {
-        MPI_Send(&min, 1, MPI_INT, 16, 0, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 15, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 13:
-    {
-        MPI_Send(&min, 1, MPI_INT, 15, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 0, 7, MPI_COMM_WORLD);
-        break;
-    }
-    case 14:
-    {
-        MPI_Send(&min, 1, MPI_INT, 16, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 17, 0, MPI_COMM_WORLD);
-        break;
-    }
-    case 15:
-    {
-        MPI_Send(&min, 1, MPI_INT, 17, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 18, 1, MPI_COMM_WORLD);
-        break;
-    }
-    case 16:
-    {
-        MPI_Send(&min, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        break;
-    }
-    case 17:
-    {
-        MPI_Send(&min, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 0, 4, MPI_COMM_WORLD);
-        break;
-    }
-    case 18:
-    {
-        MPI_Send(&min, 1, MPI_INT, 0, 5, MPI_COMM_WORLD);
-        MPI_Send(&max, 1, MPI_INT, 0, 6, MPI_COMM_WORLD);
-        break;
-    }
-    }
+    MPI_Send(&min, 1, MPI_INT, merging_scheme[my_rank][1], merging_scheme[my_rank][2], MPI_COMM_WORLD);
+    MPI_Send(&max, 1, MPI_INT,
+             merging_scheme[my_rank][3],
+             merging_scheme[my_rank][4],
+             MPI_COMM_WORLD);
 }
 
 void ReadFile(int *arr, int numElements)
@@ -156,7 +45,7 @@ int main(int argc, char *argv[])
     int my_rank;
     int *array;
     int numElements = 8;
-    int recievedNumbers[2];
+    int processInput[2];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -179,10 +68,10 @@ int main(int argc, char *argv[])
         }
     }
 
-    MPI_Recv(&recievedNumbers[0], 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, nullptr);
-    MPI_Recv(&recievedNumbers[1], 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, nullptr);
+    MPI_Recv(&processInput[0], 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, nullptr);
+    MPI_Recv(&processInput[1], 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, nullptr);
 
-    oddEvenMerge(my_rank, recievedNumbers);
+    oddEvenMerge(my_rank, processInput);
 
     if (my_rank == 0)
     {
